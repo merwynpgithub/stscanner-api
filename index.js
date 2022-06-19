5302a37994b9ee689c4e2dcc743c4766d4fc41f2
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-19 10:03:36 -0400

    add helmet and cors

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+
+app.use(cors());
+app.use(helmet());
 
 const ticker = require('./ticker');
 app.use('/ticker', ticker);
